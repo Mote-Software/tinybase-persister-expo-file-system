60681015f66d0c7afa747ab5de36046058d6ffa2
Matt Spence <admin@matt-spence.com>
2025-11-03 14:14:23 +0000

    fix(ci): broken semantic-release

---
+++ packages/tinybase-persister-expo-file-system/.releaserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false, // We'll publish manually from the workflow
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'packages/*/package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
